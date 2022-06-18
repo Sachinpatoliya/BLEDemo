0396f0bd780e48e52c3dfa86488f7c25bb385eef
Sachin Patoliya <sachinpatoliya24@gmail.com>
2022-06-18 11:49:21 +0530

    Add core bluetooth to get list of near by bluetooth devices

--- BLEDemo/Screens/BluetoothLists/ViewModel/BluetoothListViewModel.swift
+++ BLEDemo/Screens/BluetoothLists/ViewModel/BluetoothListViewModel.swift
@@ -11,5 +11,51 @@ import CoreBluetooth
 class BluetoothListViewModel: NSObject {
     var bluetoothService: BluetoothListViewModel!
 
+    var bluetoothList = [BluetoothListModel]() {
+        didSet {
+            reloadTableView?()
+        }
+    }
 
+    var manager: CBCentralManager? = nil
+    var reloadTableView: (() -> Void)?
+
+    override init(){
+        super.init()
+    }
+    
+    func getBluetoothList(){
+        manager = CBCentralManager.init(delegate: self, queue: .main)
+    }
+    
+    //Scan near by bluetooth devices
+    func scanBLEDevice(){
+        manager?.scanForPeripherals(withServices: nil, options: nil)
+    }
+}
+
+extension BluetoothListViewModel: CBCentralManagerDelegate, CBPeripheralDelegate {
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("Device: \(peripheral)")
+        print(RSSI)
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print(error!)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) { //обновление состояния менеджера
+        print(central.state)
+        switch central.state {
+        case .poweredOn:
+            scanBLEDevice()
+            break
+        default:
+            break
+        }
+    }
 }
