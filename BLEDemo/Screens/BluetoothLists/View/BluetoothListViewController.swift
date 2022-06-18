e0cdd67018212e7a2908acd8686f198c927db188
Sachin Patoliya <sachinpatoliya24@gmail.com>
2022-06-18 12:29:54 +0530

    Show near by bluetooth device in list

--- BLEDemo/Screens/BluetoothLists/View/BluetoothListViewController.swift
+++ BLEDemo/Screens/BluetoothLists/View/BluetoothListViewController.swift
@@ -12,12 +12,30 @@ class BluetoothListViewController: UIViewController {
     //MARK:- IBOutlets
     @IBOutlet weak var tblBluetoothList: UITableView!
     
+    //MARK:- Variables
+    lazy var viewModel = {
+        BluetoothListViewModel()
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tblBluetoothList.register(UINib (nibName: "BluetoothCell", bundle: nil), forCellReuseIdentifier: "BluetoothCell")
+        //Initialize View Model
+        initViewModel()
     }
     
+    func initViewModel() {
+        // Get Bluetooth List
+        viewModel.getBluetoothList()
+        
+        // Reload TableView closure
+        viewModel.reloadTableView = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tblBluetoothList.reloadData()
+            }
+        }
+    }
     
     //MARK:- Button Actions
     @IBAction func refreshAction(_ sender: UIButton){
@@ -26,3 +44,24 @@ class BluetoothListViewController: UIViewController {
     
 }
 
+
+// MARK: - UITableViewDelegate
+extension BluetoothListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension BluetoothListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.bluetoothList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BluetoothCell", for: indexPath) as? BluetoothCell else { fatalError("xib does not exists") }
+        let cellVM = viewModel.bluetoothList[indexPath.row]
+        cell.cellViewModel = cellVM
+        return cell
+    }
+}
--- BLEDemo/Screens/BluetoothLists/View/Cell/BluetoothCell.swift
+++ BLEDemo/Screens/BluetoothLists/View/Cell/BluetoothCell.swift
@@ -12,9 +12,21 @@ class BluetoothCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var RSSIidLabel: UILabel!
     
+    var cellViewModel: BluetoothListModel? {
+        didSet {
+            nameLabel.text = cellViewModel?.name == "" ? String(cellViewModel?.UUID.prefix(5) ?? "") : cellViewModel?.name ?? ""
+            RSSIidLabel.text = cellViewModel?.RSSIId.stringValue
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        RSSIidLabel.text = nil
+        nameLabel.text = nil
+    }
 }
